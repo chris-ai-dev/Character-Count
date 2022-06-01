69f1292f0ec21c643fc52f8012d31fcd96e471a1
Chris <chrisvolar@gmail.com>
2022-05-31 21:05:54 -0500

    work but test cases

--- python/char_count.py
+++ python/char_count.py
@@ -1,2 +1,27 @@
 def char_count(str):
   # Your code here
+  #create empty dict/ob
+  tracker = {}
+  list_tracker = []
+  
+  #iterate through tracker to append letter to tracker
+  for character in str:
+    # tracker['a'] += 1
+    # #check if character is None/undefined meaning it doesn't exist
+    if character == " ":
+      continue
+    if tracker.get(character) == None:
+      tracker[character] = 1
+    else:
+      tracker[character] += 1
+  #turn into list format 
+  for key, value in tracker.items():
+    temp = [key,value]
+    list_tracker.append(temp)    
+  list_tracker.sort()    
+  return list_tracker
+  
+  
+  
+  
+print(char_count("an apple a day will keep the doctor away"))
